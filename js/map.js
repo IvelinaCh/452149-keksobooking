ee89c74395ad98ec301063af062477c9c0fd7659
IvelinaCh <lynnln91@gmail.com>
2017-11-27 11:44:50 +0300

    create map v2

--- js/map.js
+++ js/map.js
@@ -3,8 +3,11 @@
 var avatars = [1, 2, 3, 4, 5, 6, 7, 8];
 var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var types = ['flat', 'house', 'bungalo'];
-var featuress = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var times = ['12:00', '13:00', '14:00'];
+var mapTemplate = document.querySelector('template').content;
+var mapPins = document.querySelector('.map__pins');
+
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -16,25 +19,61 @@ var getRandomFromArray = function (arr) {
 };
 
 var createHome = function () {
-  var home = {
+  return {
     author: {
-      vatar: '../img/avatars/user0' + getRandomFromArray(avatars) + '.png'
+      avatar: '../img/avatars/user0' + getRandomFromArray(avatars) + '.png'
     },
     offer: {
       title: getRandomFromArray(titles),
-      address: "{{location.x}}, {{location.y}}",
-      price: getRandom(1000, 1 000 001),
+      address: x + ', ' + y,
+      price: getRandom(1000, 1000001),
       type: getRandomFromArray(types),
       rooms: getRandom(1, 6),
-      guests: getRandom(),
+      guests: getRandom(1, 8),
       checkin: getRandomFromArray(times),
       checkout: getRandomFromArray(times),
-      features: getRandomFromArray(featuress),
+      features: getRandomFromArray(features),
       description: '',
       photos: []
     },
     location: {
-      x: getRandom(300, 901),
-      y: getRandom(100, 501)
+      x: x,
+      y: y
     }
+  }
 }
+
+var createHomes = function (homesCount) {
+  var homes = [];
+  for (var i = 0; i < homesCount; i++) {
+    homes.push(createHome());
+  }
+  return homes;
+}
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var x = getRandom(300, 901);
+var y = getRandom(100, 501);
+var homesCount = 8;
+var homes = createHomes(homesCount);
+
+var createPin = function (homesCount) {
+  var pinElement = mapTemplate.cloneNode(true);
+  var button = pinElement.querySelector('.map__pin');
+  var pin = button.querySelector('img');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < homesCount; i++) {
+    button.style.left = x;
+    button.style.top = y;
+    pin = homes[i].author.avatar;
+
+    fragment.appendChild(button);
+  }
+
+  return fragment;
+}
+
+mapPins.appendChild(createPin(homesCount));
+
