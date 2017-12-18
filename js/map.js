ee747dd2d8c912f0569b3ad2d27a7d56ace771b2
IvelinaCh <lynnln91@gmail.com>
2017-12-18 21:52:24 +0300

    filters v1

--- js/map.js
+++ js/map.js
@@ -3,5 +3,24 @@
 window.map = (function (cardModule, pinModule) {
   var map = cardModule.map;
 
+var mapFilterForm = document.querySelector('.map__filters');
+var mapFilter = document.querySelectorAll('.map__filter');
+var housingType = mapFilterForm.querySelector('#housing-type');
+var housingPrice = mapFilterForm.querySelector('#housing-price');
+var housingRooms = mapFilterForm.querySelector('#housing-rooms');
+var mapFilterhousingGuests = mapFilterForm.querySelector('#housing-guests');
+var housingFeature = mapFilterForm.querySelectorAll('.feature');
+
+var mapFilters = Array.from(mapFilter);
+var housingFeatures = Array.from(housingFeature);
+var allFilters = mapFilters.concat(housingFeatures);console.log(allFilters[0].housingRooms);
+
   pinModule.addMainPinEvent(map);
+
+
+
 })(window.card, window.pin);
+
+
+/*if () {}
+*/
